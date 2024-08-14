0b43b07b9cc7feacde7312146505505549e3fc0b
jayasimhagit <jayasimha.xyz@gmail.com>
2024-08-14 11:09:52 +0530

    Second Commit 1 files

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "random_pet" "rg_name" {
   prefix = var.resource_group_name_prefix
 }
-
+#This is added code..
 resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
   name     = random_pet.rg_name.id
